07a80e09c8d51b29b99c3da967c1badeabe6a6e6
aleleotta <114069815+aleleotta@users.noreply.github.com>
2024-05-27 21:41:41 +0200

    .

--- DAL/clsListadosDAL.cs
+++ DAL/clsListadosDAL.cs
@@ -9,5 +9,14 @@ namespace DAL
 {
     public class clsListadosDAL
     {
+        public List<clsPersona> getListadoCompletoPersonas()
+        {
+            return new List<clsPersona>();
+        }
+        public List<clsDepartamento> getListadoCompletoDepartamentos()
+        {
+            return new List<clsDepartamento>();
+        }
+        public void updateListadoPersonas(List<clsPersona> listadoPersonasConCambio) { }
     }
 }
