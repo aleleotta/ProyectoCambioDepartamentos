52f2d9a1ccffcd2f58aa26e2135acbd26ee45c15
aleleotta <114069815+aleleotta@users.noreply.github.com>
2024-05-29 10:32:20 +0200

    .

--- DAL/clsListadosDAL.cs
+++ DAL/clsListadosDAL.cs
@@ -9,30 +9,97 @@ using Microsoft.Data.SqlClient;
 namespace DAL
 {
     public class clsListadosDAL
-    {/// <summary>
-    /// Accede a la base de datos para recojer todos las personas y devolverlas como un listado.
-    /// </summary>
-    /// <returns>El listado de personas</returns>
+    {
+        /// <summary>
+        /// Accede a la base de datos para recojer todas las personas y devolverlas como un listado.
+        /// </summary>
+        /// <returns>El listado de personas</returns>
         public List<clsPersona> getListadoCompletoPersonas()
         {
             List<clsPersona> listadoPersonas = new List<clsPersona>();
             SqlConnection conn = new SqlConnection();
             conn.ConnectionString = clsConnection.getConnection();
-            SqlCommand cmd = null;
+            SqlCommand cmd;
+            SqlDataReader reader;
             try
             {
-
+                conn.Open();
+                cmd = conn.CreateCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "SELECT * FROM Personas";
+                reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        clsPersona persona = new clsPersona((int)reader["ID"], (string)reader["Nombre"], (string)reader["Apellidos"], (int)reader["IDDepartamento"]);
+                        listadoPersonas.Add(persona);
+                    }
+                }
+                conn.Close();
             }
             catch (Exception err)
             {
                 listadoPersonas = new List<clsPersona>();
+                Console.WriteLine(err.Message);
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
             }
             return listadoPersonas;
         }
+
+        /// <summary>
+        /// Accede a la base de datos para recojer todos los departamentos y devolverlos como un listado.
+        /// </summary>
+        /// <returns>El listado de departamentos</returns>
         public List<clsDepartamento> getListadoCompletoDepartamentos()
         {
-            return new List<clsDepartamento>();
+            List<clsDepartamento> listadoDepartamentos = new List<clsDepartamento>();
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = clsConnection.getConnection();
+            SqlCommand cmd;
+            SqlDataReader reader;
+            try
+            {
+                conn.Open();
+                cmd = conn.CreateCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "SELECT * FROM Departamentos";
+                reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        clsDepartamento dept = new clsDepartamento((int)reader["ID"], (string)reader["Nombre"]);
+                        listadoDepartamentos.Add(dept);
+                    }
+                }
+                conn.Close();
+            }
+            catch (Exception err)
+            {
+                listadoDepartamentos = new List<clsDepartamento>();
+                Console.WriteLine(err.Message);
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+            return listadoDepartamentos;
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="listadoPersonasConCambio"></param>
         public void updateListadoPersonas(List<clsPersona> listadoPersonasConCambio) { }
     }
 }
